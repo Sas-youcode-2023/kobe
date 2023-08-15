7445d6bcff491f053169c309a2c8fd983e9d2717
kobe <142225676+kobe24mamaba@users.noreply.github.com>
2023-08-15 10:56:07 +0100

    Update main.c
    finding the max number in you phone number with his INDEX

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int phonenumber_nums[10];
+    int i;
+    int higher_num;
+    int index;
+    printf("set your phone number : ");
+    for(i=0;i<=9;i++){
+            scanf("%d", &phonenumber_nums[i]);
+    }
+    higher_num=phonenumber_nums[0];
+    for(i=0;i<= 9;i++){
+        if (phonenumber_nums[i]>=higher_num){
+            higher_num=phonenumber_nums[i];
+            index=i;
+        }
+
+    }
+    printf("The higher num is : %d . With the index : %d",higher_num,index);
+    return 0;
+}
