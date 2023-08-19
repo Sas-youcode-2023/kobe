f03afc9b9cb1c5764c9e8024fce77e79d33aa510
kobe24mamaba <142225676+kobe24mamaba@users.noreply.github.com>
2023-08-19 03:06:42 +0100

    Update char_swap.c
    edit random letter position

--- char_swap.c
+++ char_swap.c
@@ -17,42 +17,44 @@ int main() {
             printf("s index is: %d\n", i);
             s_index = i; // Set the index, but only if it's the first occurrence
         }
-        
+
         if (m_index != -1 && s_index != -1) {
             break;  // Break the loop if both indices are found
         }
     }
-    
+
     if (m_index != -1 && s_index != -1) {
         // Swap the characters at 'm' and 's' indices
         char temp = sentence[m_index];
         sentence[m_index] = sentence[s_index];
         sentence[s_index] = temp;
-        
+
         printf("\nSwapped sentence: %s\n\n", sentence);
     } else {
         printf("'m' or 's' not found in the sentence.\n");
     }
-    
+
     // Prompt user to input a random letter
     char random_letter;
     printf("Set a random letter between A-Z : ");
     scanf(" %c", &random_letter);
-    
+
     int scanner = 0;
-    // Count occurrences of the random letter in the sentence
+    // Find the first position of the random letter in the sentence
+    int first_position = -1;
     for (i = 0; i < size; i++) {
         if (sentence[i] == random_letter) {
-            scanner++;
+            first_position = i;
+            break;  // Break after finding the first occurrence
         }
     }
-    
+
     // Check and display whether the random letter is found in the sentence
-    if (scanner > 0) {
-        printf("YES !!!, I found \"%c\" in the sentence\n\n", random_letter);
+    if (first_position != -1) {
+        printf("YES !!!, I found \"%c\" in the sentence with the position %d\n\n", random_letter,first_position);
     } else {
-        printf("\nNO !!!, \"%c\" not found in the sentence.\n\n", random_letter);
+        printf("\n'%c' not found in the sentence.\n", random_letter);
     }
-    
+
     return 0;
 }
